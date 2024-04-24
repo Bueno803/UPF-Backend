1a686f0781abbd8bdad62342a6f7ce7bbf30dc22
Abdullah Bueno <abueno@email.sc.edu>
2024-04-24 12:24:59 -0700

    started create client endpoint
    started create client endpoint

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,41 +1,50 @@
-import { Body, Controller, Get, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { ClientsService } from './clients.service';
 
 @Controller('clients')
 export class ClientsController {
+  constructor(private clientsService: ClientsService) {}
 
-    constructor(private clientsService: ClientsService) {}
+  @Get('/all')
+  async getClients(@Res() response) {
+    try {
+    } catch (error) {}
+  }
 
-    @Get('/all')
-    async getClients(@Res() response) {
-        try {
-            
-        } catch (error) {
-            
-        }
+  @Post('/create')
+  async createClient(
+    @Res() response,
+    @Req() req: Request,
+    @Body() userDto: any,
+  ) {
+    try {
+      const clients = await this.clientsService
+        .createClient(userDto)
+        .then(() => {
+          // console.log("resUser: ", resClient);
+          // if(resClient.resData == 'user exists') {
+          //     return response.status(HttpStatus.CONFLICT).json({
+          //         message: 'User already exists',
+          //         data: resClient.client
+          //     });
+          // } else {
+          //     return response.status(HttpStatus.OK).json({
+          //         message: 'User successfully created',
+          //         resClient
+          //     });
+          // }
+        });
+    } catch (error) {
+      console.log('error: ', error);
     }
-
-    @Post('/create')
-    async createClient(@Res() response, @Req() req: Request, @Body() userDto: any) {
-        try {
-            const clients = await this.clientsService.createClient(userDto).then((resClient) => {
-                console.log("resUser: ", resClient);
-
-                if(resClient.resData == 'user exists') {
-                    return response.status(HttpStatus.CONFLICT).json({
-                        message: 'User already exists',
-                        data: resClient.client
-                    });
-                } else {
-                    return response.status(HttpStatus.OK).json({
-                        message: 'User successfully created',
-                        resClient
-                    });
-                }
-            });
-        } catch (error) {
-            console.log("error: ", error);
-        }
-        return null
-    }
+    return null;
+  }
 }
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,30 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+// import { collection, getDocs, query } from 'firebase/firestore';
 
 @Injectable()
 export class ClientsService {
+  constructor() {}
 
-    constructor() {}
-
-    async createClient(clientInfo: any): Promise<any> {
-        // check user existence?
-        try {
-            const docRef = await admin.firestore().collection('upf-db').doc('client_space');
-            await docRef.get().then(async (clients) => {
-                clients.data().data.forEach((client) => {
-                    if(clientInfo.firstname == client.FirstName && clientInfo.lastname == client.LastName) {
-                        return {resData: 'user exists', client: client}
-                    }
-                    console.log("individual clients: ", client);
-                })
-                // console.log("3 ", clients.data().data[0]);
-                // console.log("1 ", clients);
-                await docRef.set(clientInfo).then(() => {
-                    return { resData: 'success'};
-                })
-            })
-        } catch (error) {
-            console.log("An error occured creating client: ", error);
-        }
+  async createClient(clientInfo: any): Promise<any> {
+    // check user existence?
+    try {
+      const docRef = await admin.firestore().collection('client_space');
+      await docRef
+        .where('FirstName', '==', clientInfo.FirstName)
+        .get()
+        .then((result) => {
+          result.forEach((doc) => {
+            console.log('document: ', doc);
+          });
+        });
+      //   const quierySnapshot = await getDocs(result);
+      //   console.log('result: ', quierySnapshot);
+      //   await docRef.get().then(async (clients) => {
+      //     clients.data().data.forEach((client) => {
+      //       if (
+      //         clientInfo.firstname == client.FirstName &&
+      //         clientInfo.lastname == client.LastName
+      //       ) {
+      //         return { resData: 'user exists', client: client };
+      //       }
+      //       console.log('individual clients: ', client);
+      //     });
+      //     // console.log("3 ", clients.data().data[0]);
+      //     // console.log("1 ", clients);
+      //     await docRef.set(clientInfo).then(() => {
+      //       return { resData: 'success' };
+      //     });
+      //   });
+    } catch (error) {
+      console.log('An error occured creating client: ', error);
     }
+  }
 }
