66b5735a57df3e3c8e9ebdb8855b14b5e10c391d
Abdullah Bueno <abueno@email.sc.edu>
2024-05-28 11:28:35 -0700

    update class in progress

--- src/class-in-progress/class-in-progress.service.ts
+++ src/class-in-progress/class-in-progress.service.ts
@@ -30,18 +30,21 @@ export class ClassInProgressService {
   async getClassStamps() {
     try {
       const response = [];
-      let TechInfo;
-      const docRef = await admin.firestore().collection('last_class_time_stamp');
+      let TechInfo = null;
+      const docRef = await admin
+        .firestore()
+        .collection('last_class_time_stamp');
       await docRef.get().then(async (result) => {
         result.forEach((doc) => {
           response.push(doc.data());
         });
       });
-         TechInfo = { lilInfo: await this.getLilTechInfo(),
-          advInfo: await this.getAdvTechInfo()
-         } 
-      console.log("TechInfo ", TechInfo);
-      return {classStamp: response, TechInfo: TechInfo};
+      TechInfo = {
+        lilInfo: await this.getLilTechInfo(),
+        advInfo: await this.getAdvTechInfo(),
+      };
+      console.log('TechInfo ', TechInfo);
+      return { classStamp: response, TechInfo: TechInfo };
     } catch (error) {
       console.log('An error occurred fetching class stamps: ', error);
       return error;
@@ -54,43 +57,56 @@ export class ClassInProgressService {
       const handAtksResponse = [];
       const blocksResponse = [];
       const kicksResponse = [];
-      const formsDocRef = await admin.firestore().collection('lil_formstid_info');
-      const handAtksDocRef = await admin.firestore().collection('lil_handatkstid_info');
-      const blocksDocRef = await admin.firestore().collection('lil_blockstid_info');
-      const kicksDocRef = await admin.firestore().collection('lil_kickstid_info');
+      const formsDocRef = await admin
+        .firestore()
+        .collection('lil_formstid_info');
+      const handAtksDocRef = await admin
+        .firestore()
+        .collection('lil_handatkstid_info');
+      const blocksDocRef = await admin
+        .firestore()
+        .collection('lil_blockstid_info');
+      const kicksDocRef = await admin
+        .firestore()
+        .collection('lil_kickstid_info');
 
       await formsDocRef.get().then(async (result) => {
-        console.log("result ", result)
+        console.log('result ', result);
         result.forEach((doc) => {
-          console.log("doc ",doc);
+          console.log('doc ', doc);
           formsResponse.push(doc.data());
         });
       });
 
       await handAtksDocRef.get().then(async (result) => {
-        console.log("result ", result)
+        console.log('result ', result);
         result.forEach((doc) => {
-          console.log("doc ",doc);
+          console.log('doc ', doc);
           handAtksResponse.push(doc.data());
         });
       });
 
       await blocksDocRef.get().then(async (result) => {
-        console.log("result ", result)
+        console.log('result ', result);
         result.forEach((doc) => {
-          console.log("doc ",doc);
+          console.log('doc ', doc);
           blocksResponse.push(doc.data());
         });
       });
 
       await kicksDocRef.get().then(async (result) => {
-        console.log("result ", result)
+        console.log('result ', result);
         result.forEach((doc) => {
-          console.log("doc ",doc);
+          console.log('doc ', doc);
           kicksResponse.push(doc.data());
         });
       });
-      return {forms: formsResponse, handAtks: handAtksResponse, blocks: blocksResponse, kicks: kicksResponse};
+      return {
+        forms: formsResponse,
+        handAtks: handAtksResponse,
+        blocks: blocksResponse,
+        kicks: kicksResponse,
+      };
     } catch (error) {
       console.log('An error occurred fetching class stamps: ', error);
       return error;
@@ -103,43 +119,56 @@ export class ClassInProgressService {
       const handAtksResponse = [];
       const blocksResponse = [];
       const kicksResponse = [];
-      const formsDocRef = await admin.firestore().collection('adv_formstid_info');
-      const handAtksDocRef = await admin.firestore().collection('adv_handatkstid_info');
-      const blocksDocRef = await admin.firestore().collection('adv_blockstid_info');
-      const kicksDocRef = await admin.firestore().collection('adv_kickstid_info');
+      const formsDocRef = await admin
+        .firestore()
+        .collection('adv_formstid_info');
+      const handAtksDocRef = await admin
+        .firestore()
+        .collection('adv_handatkstid_info');
+      const blocksDocRef = await admin
+        .firestore()
+        .collection('adv_blockstid_info');
+      const kicksDocRef = await admin
+        .firestore()
+        .collection('adv_kickstid_info');
 
       await formsDocRef.get().then(async (result) => {
-        console.log("result ", result)
+        console.log('result ', result);
         result.forEach((doc) => {
-          console.log("doc ",doc);
+          console.log('doc ', doc);
           formsResponse.push(doc.data());
         });
       });
 
       await handAtksDocRef.get().then(async (result) => {
-        console.log("result ", result)
+        console.log('result ', result);
         result.forEach((doc) => {
-          console.log("doc ",doc);
+          console.log('doc ', doc);
           handAtksResponse.push(doc.data());
         });
       });
 
       await blocksDocRef.get().then(async (result) => {
-        console.log("result ", result)
+        console.log('result ', result);
         result.forEach((doc) => {
-          console.log("doc ",doc);
+          console.log('doc ', doc);
           blocksResponse.push(doc.data());
         });
       });
 
       await kicksDocRef.get().then(async (result) => {
-        console.log("result ", result)
+        console.log('result ', result);
         result.forEach((doc) => {
-          console.log("doc ",doc);
+          console.log('doc ', doc);
           kicksResponse.push(doc.data());
         });
       });
-      return {forms: formsResponse, handAtks: handAtksResponse, blocks: blocksResponse, kicks: kicksResponse};
+      return {
+        forms: formsResponse,
+        handAtks: handAtksResponse,
+        blocks: blocksResponse,
+        kicks: kicksResponse,
+      };
     } catch (error) {
       console.log('An error occurred fetching class stamps: ', error);
       return error;
@@ -164,33 +193,30 @@ export class ClassInProgressService {
 
   async updateStudentReady(data: any) {
     try {
-      const docRef = await admin
-        .firestore()
-        .collection('test_ready');
+      const docRef = await admin.firestore().collection('test_ready');
 
-        switch (data.readyField) {
-          case 'FormsRdy': {
-            docRef.doc(data.ClientID).update({FormsRdy: data.isReady});
-            break;
-          }
-          case 'BlocksRdy': {
-            docRef.doc(data.ClientID).update({BlocksRdy: data.isReady});
-            break;
-          }
-          case 'HandAtksRdy': {
-            docRef.doc(data.ClientID).update({HandAtksRdy: data.isReady});
-            break;
-          }
-          case 'KickssRdy': {
-            docRef.doc(data.ClientID).update({KickssRdy: data.isReady});
-            break;
-          }
+      switch (data.readyField) {
+        case 'FormsRdy': {
+          docRef.doc(data.ClientID).update({ FormsRdy: data.isReady });
+          break;
         }
-        return {status: "success"};
-        
+        case 'BlocksRdy': {
+          docRef.doc(data.ClientID).update({ BlocksRdy: data.isReady });
+          break;
+        }
+        case 'HandAtksRdy': {
+          docRef.doc(data.ClientID).update({ HandAtksRdy: data.isReady });
+          break;
+        }
+        case 'KickssRdy': {
+          docRef.doc(data.ClientID).update({ KickssRdy: data.isReady });
+          break;
+        }
+      }
+      return { status: 'success' };
     } catch (error) {
-      console.log("An error occurred updating student readiness: ", error);
-      return {status: error};
+      console.log('An error occurred updating student readiness: ', error);
+      return { status: error };
     }
   }
 
